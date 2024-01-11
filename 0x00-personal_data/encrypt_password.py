87173b8e744aa213a58c2ce4c7a54ab84baf3a43
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-11 19:45:05 +0200

    0x00 - Import Changes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Module for password encryption using bcrypt."""
 
-import bcrypt
+from bcrypt import hashpw, gensalt, checkpw
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a plaintext password using bcrypt."""
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    """Hashes a plaintext password."""
+    return hashpw(password.encode("utf-8"), gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Validates a plaintext password against a hashed password using bcrypt"""
-    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+    """Validates a plaintext password against a hashed password."""
+    return checkpw(password.encode("utf-8"), hashed_password)
