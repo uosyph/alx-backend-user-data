bfe84eb7a5aa3ef430022449ef817ec442de50a4
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-16 18:23:15 +0200

    0x01 - Tasks (7,8)

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Module providing Basic Authentication functionality."""
 
+import base64
+import re
+from typing import TypeVar
+from models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -10,3 +14,45 @@ class BasicAuth(Auth):
     Parameters:
         Auth (Type): Inherits from Auth class.
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header.
+
+        Parameters:
+            authorization_header (str): The Authorization header.
+
+        Returns:
+            str: The Base64 part of the Authorization header.
+        """
+        if (
+            not authorization_header
+            or not isinstance(authorization_header, str)
+        ):
+            return None
+
+        if re.search("^Basic ", authorization_header):
+            return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Decodes the Base64 Authorization header.
+
+        Parameters:
+            base64_authorization_header (str): The Base64 Authorization header.
+
+        Returns:
+            str: Decoded value of the Base64 string.
+        """
+        if (
+            not base64_authorization_header
+            or not isinstance(base64_authorization_header, str)
+        ):
+            return None
+
+        try:
+            return base64.b64decode(
+                base64_authorization_header.encode("utf-8")).decode("utf-8")
+        except Exception:
+            return None
