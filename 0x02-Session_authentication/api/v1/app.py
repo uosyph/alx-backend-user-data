490ff165c61e30c9acbae141c0b909ad1cafc183
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-17 23:19:42 +0200

    0x02 - Minor Fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,7 +9,6 @@ from flask_cors import CORS, cross_origin
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
@@ -21,10 +20,8 @@ if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
-elif getenv('AUTH_TYPE') == 'session_auth':
+elif getenv("AUTH_TYPE") == "session_auth":
     auth = SessionAuth()
-elif getenv('AUTH_TYPE') == 'session_exp_auth':
-    auth = SessionExpAuth()
 
 
 @app.before_request
