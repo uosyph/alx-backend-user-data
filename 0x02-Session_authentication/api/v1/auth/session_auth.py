133ce64d4c4ef921928db2485aba91241e776a16
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-17 18:42:19 +0200

    0x02 - Tasks (3,6)

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,3 +32,33 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieve the user ID associated with a session ID.
+
+        Parameters:
+            session_id (str, optional): The session ID. Defaults to None.
+
+        Returns:
+            str: The user ID associated with the session ID.
+        """
+        if not session_id or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Retrieve the current user based on the session cookie.
+
+        Parameters:
+            request (object, optional): The request object. Defaults to None.
+
+        Returns:
+            User: The current User object if a valid session exists,
+            otherwise None.
+        """
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return None
+
+        return User.get(self.user_id_for_session_id(session_id))
