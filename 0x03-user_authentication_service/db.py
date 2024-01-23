18d287af535a29c769d3f10e36468b798f3cef07
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-23 11:13:14 +0200

    0x03 - Tasks (1,2)

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,47 @@
+"""DB module"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+
+from user import Base, User
+
+
+class DB:
+    """DB class"""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
+        """Add new user"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kargs) -> TypeVar("User"):
+        """Find user"""
+        try:
+            user = self._session.query(User).filter_by(**kargs).first()
+            if not user:
+                raise NoResultFound
+        except TypeError:
+            raise InvalidRequestError
+        return user
