5b86ebff61d2479e9ca6bb5b376263ccd29ffa20
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-24 00:05:17 +0200

    0x03 - 1-3: Changes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,57 +4,62 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
+from typing import TypeVar
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB Class for Object Rational Mapping"""
 
-    def __init__(self) -> None:
+    def __init__(self):
         """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object"""
+    def _session(self):
+        """Session Getter Method"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add new user"""
+        """Adds a new user to database Return and return its object"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find user"""
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-        except TypeError:
+        """Return: First row found in the users table, filtered by kwargs"""
+        if not kwargs:
             raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Update user"""
+        """Updates users attributes"""
         user = self.find_user_by(id=user_id)
 
-        for key, value in kwargs.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
-            else:
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
                 raise ValueError
 
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+
         self._session.commit()
-        return None
