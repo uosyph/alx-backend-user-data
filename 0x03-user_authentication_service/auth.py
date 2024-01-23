2c399bf6d476ebf885d265a876aa7606b426ad6c
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-23 23:45:33 +0200

    0x03 - Tasks (4,5,8,9)

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" Authentication
+"""
+
+from bcrypt import hashpw, gensalt, checkpw
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from uuid import uuid4
+from typing import Union
+
+from db import DB
+from user import User
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Takes in string, converts to unicode
+    Returns salted and hashed passwd as a bytestring
+    """
+    return hashpw(password.encode("utf-8"), gensalt())
+
+
+def _generate_uuid() -> str:
+    """Returns string representation of new UUID"""
+    return str(uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        """Instance"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers and returns a new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if user login is valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode("utf-8"), user.hashed_password)
+        except NoResultFound:
+            return False
