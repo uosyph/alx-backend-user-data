32d05a12940fe1ff7492b7d3f3883bc5dd1e5360
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-24 07:29:32 +0200

    0x03 - Tasks (11,14)

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -31,5 +31,40 @@ def register() -> str:
     return jsonify({"email": email, "message": "user created"})
 
 
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """Logs in a user and returns session ID"""
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+    except KeyError:
+        abort(400)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout() -> str:
+    """Logs out the user if logged in"""
+    session_id = request.cookies.get("session_id", None)
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect("/")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
