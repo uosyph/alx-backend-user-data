4639fbef544d9bd33d2e0313f672836b973eafc2
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-24 06:49:04 +0200

    0x03 - Tasks (16,18)

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,3 +86,28 @@ class Auth:
 
         self._db.update_user(user.id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Uses reset token to validate update of user's password"""
+        if reset_token is None or password is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password, reset_token=None)
