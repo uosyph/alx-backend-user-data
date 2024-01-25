28245df7632eca5a55a2eec4152fd65f0d6a7dde
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-25 04:07:40 +0200

    0x03 - Improve docstring

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-"""API Routes for Authentication Service"""
+"""API Routes for Authentication Service
+
+This script defines the API routes for an Authentication Service using Flask.
+The service provides functionality for user registration, login, logout,
+profile retrieval, password reset token generation, and password update.
+"""
 
 from auth import Auth
 from flask import Flask, jsonify, request, abort, redirect
@@ -10,13 +15,21 @@ AUTH = Auth()
 
 @app.route("/", methods=["GET"])
 def index() -> str:
-    """Base route for authentication service API"""
+    """Base route for authentication service API
+
+    Returns:
+        str: A JSON response with a welcome message.
+    """
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route("/users", methods=["POST"])
 def register() -> str:
-    """Registers a new user if it does not exist before"""
+    """Registers a new user if it doesn't already exist.
+
+    Returns:
+        str: A JSON response with the email of the registered user.
+    """
     try:
         email = request.form["email"]
         password = request.form["password"]
@@ -33,7 +46,11 @@ def register() -> str:
 
 @app.route("/sessions", methods=["POST"])
 def login() -> str:
-    """Logs in a user and returns session ID"""
+    """Logs in a user and returns the session ID.
+
+    Returns:
+        str: A JSON response with the email of the logged-in user.
+    """
     try:
         email = request.form["email"]
         password = request.form["password"]
@@ -52,7 +69,11 @@ def login() -> str:
 
 @app.route("/sessions", methods=["DELETE"])
 def logout() -> str:
-    """Logs out the user if logged in"""
+    """Logs out the user if logged in.
+
+    Returns:
+        str: A redirect to the base route ("/") after logging out.
+    """
     session_id = request.cookies.get("session_id", None)
     if session_id is None:
         abort(403)
@@ -68,7 +89,11 @@ def logout() -> str:
 
 @app.route("/profile", methods=["GET"])
 def profile() -> str:
-    """return 200 if user exist, otherwise 403."""
+    """Checks if user exists and responds correspondingly.
+
+    Returns:
+        str: A JSON response with the email of the user if it exists.
+    """
     session_id = request.cookies.get("session_id", None)
     if session_id is None:
         abort(403)
@@ -82,7 +107,12 @@ def profile() -> str:
 
 @app.route("/reset_password", methods=["POST"])
 def reset_password() -> str:
-    """Generates a token if user exists, otherwise return 403."""
+    """Generates a token if the user exists; otherwise, returns 403.
+
+    Returns:
+        str: A JSON response with the email of
+        the user and the generated reset token.
+    """
     try:
         email = request.form["email"]
     except KeyError:
@@ -98,7 +128,11 @@ def reset_password() -> str:
 
 @app.route("/reset_password", methods=["PUT"])
 def update_password() -> str:
-    """Updates password with a reset token"""
+    """Updates the password using a reset token.
+
+    Returns:
+        str: A JSON response with the email of the user.
+    """
     try:
         email = request.form["email"]
         reset_token = request.form["reset_token"]
