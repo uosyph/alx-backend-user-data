9a9891c606b3d58178400c45f123cd7dfb425c96
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-25 03:54:28 +0200

    0x03 - Tasks (15,17,19)

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,5 +66,53 @@ def logout() -> str:
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"])
+def profile() -> str:
+    """return 200 if user exist, otherwise 403."""
+    session_id = request.cookies.get("session_id", None)
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    return jsonify({"email": user.email}), 200
+
+
+@app.route("/reset_password", methods=["POST"])
+def reset_password() -> str:
+    """Generates a token if user exists, otherwise return 403."""
+    try:
+        email = request.form["email"]
+    except KeyError:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password() -> str:
+    """Updates password with a reset token"""
+    try:
+        email = request.form["email"]
+        reset_token = request.form["reset_token"]
+        new_password = request.form["new_password"]
+    except KeyError:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
